28fd8f86e4aac4a86bf939191c51ddecded93b5c
Travis Mix <travismix1980@gmail.com>
2023-07-20 15:49:38 -0700

    Did some variable stuff

---
+++ GettingStartedWithJava/src/java_basics/Variables.java
@@ -0,0 +1,18 @@
+package java_basics;
+
+public class Variables {
+
+	public static void main(String[] args) {
+		int iMaxValue = Integer.MAX_VALUE;
+		int iMinValue = Integer.MIN_VALUE;
+		double dMaxValue = Double.MAX_VALUE;
+		double dMinValue = Double.MIN_VALUE;
+		String helloString = "Hello";
+		System.out.println(iMaxValue);
+		System.out.println(iMinValue);
+		System.out.println(dMaxValue);
+		System.out.println(dMinValue);
+		System.out.println(helloString);
+	}
+
+}
