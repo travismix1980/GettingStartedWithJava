5105d360606631871f90fff23af7af8e8d1b1042
Travis Mix <travismix1980@gmail.com>
2023-07-20 16:02:14 -0700

    started with control flow

---
+++ GettingStartedWithJava/src/java_basics/ControlFlow.java
@@ -0,0 +1,14 @@
+package java_basics;
+
+public class ControlFlow {
+
+	public static void main(String[] args) {
+		boolean hungry = true;
+		
+		if(hungry) {
+			System.out.println("I'm starving...");
+		}else {
+			System.out.println("I'm not hungry");
+		}
+	}
+}
