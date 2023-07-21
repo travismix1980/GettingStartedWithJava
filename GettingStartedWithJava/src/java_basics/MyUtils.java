faa6bc360b4905d9eb3527f4bb1d3a86e81e21b8
Travis Mix <travismix1980@gmail.com>
2023-07-21 02:23:11 -0700

    Finished with basics of methods

---
+++ GettingStartedWithJava/src/java_basics/MyUtils.java
@@ -0,0 +1,20 @@
+package java_basics;
+
+public class MyUtils {
+	
+	public static void printSomeJunk(String whatToPrint) {
+		System.out.println(whatToPrint);
+	}
+	
+	public static void printSomeJunk(int whatToPrint) {
+		System.out.println(whatToPrint);
+	}
+	
+	public static void sumTwoNumbers(int firstArg, int secondArg) {
+		System.out.println(firstArg + secondArg);
+	}
+	
+	public static int addTen(int someArgument) {
+		return someArgument + 10;
+	}
+}
